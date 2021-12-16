c4c1990b2c278f85edeb1dd6df7122df08d0b028
Fazliddin-TM <dasturchiiio8o7@gmail.com>
2021-12-16 12:30:32 +0300

    master

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import React from 'react';
-import "./App.css"
-import Home from './home';
-import About from './about';
-import Service from './service';
-import Project from './project';
-import Contact from './contact';
-import Footer from './Footer';
+import React from "react";
+import "./App.css";
+import Home from "./home";
+import About from "./about";
+import Service from "./service";
+import Project from "./project";
+import Contact from "./contact";
+import Footer from "./Footer";
 function App() {
   return (
     <div className="App">
-      <Home/>
-      <About/>
-      <Service/>
-      <Project/>
-      <Contact/>
-      <Footer/>
-          </div>
+      <Home />
+      <About />
+      <Service />
+      <Project />
+      <Contact />
+      <Footer />
+    </div>
   );
 }
 
